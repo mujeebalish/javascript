ce1d91d3d9906c36edb16d8ee8d8551d1c1afe20
mujeebalish <mujeebrehman12122@gmail.com>
2024-03-01 09:18:56 -0800

    javascript

---
+++ app.js
@@ -0,0 +1,20 @@
+                                document.write("<h1>Babymodernlife</h1>")
+         // 1st Function
+//alert("Welcome to my fist website")
+    //  Data types 
+    //  "Babymodernlife" this is String
+    //  1,2,3, 123 this is integer 
+    //  1.2 1.3 this is float
+    // types of error
+// 1.uncaught refferanceError: is not defined
+//  Exp: alert(Babymodernlife)
+// 2.uncaught syntaxError: missing +
+// Exp:2nd Function
+// var websitename = "www.Babymodernlife.com"
+        //alert("websitename")    //wrong 
+// alert(websitename)      //ok 
+var websitename2 = "www.AlishWDev.com"
+alert(websitename2)
+//  2nd Function 
+// document.write('<h1>this is your your time</h1>') //ok
+// document.write('<h1>'+'www.AlishWDev.com'+'</h1>') //2nd type
