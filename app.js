e3674028b40220efe2dbde446e4584eb209eca85
mujeebalish <mujeebrehman12122@gmail.com>
2024-03-04 04:06:32 -0800

    10% done

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
                                 document.write("<h1>Babymodernlife</h1>")
+
          // 1st Function
 //alert("Welcome to my fist website")
+
+
     //  Data types 
     //  "Babymodernlife" this is String
     //  1,2,3, 123 this is integer 
     //  1.2 1.3 this is float
+
+
     // types of error
 // 1.uncaught refferanceError: is not defined
 //  Exp: alert(Babymodernlife)
 // 2.uncaught syntaxError: missing +
 // Exp:2nd Function
+
+// vareable for String
 // var websitename = "www.Babymodernlife.com"
         //alert("websitename")    //wrong 
 // alert(websitename)      //ok 
-var websitename2 = "www.AlishWDev.com"
-alert(websitename2)
-//  2nd Function 
-// document.write('<h1>this is your your time</h1>') //ok
+// var websitename2 = "www.AlishWDev.com"
+// alert(websitename2)
+
+
+//  2nd Function document.write
+// document.write('<h1>this is your your time baby</h1>') //ok
 // document.write('<h1>'+'www.AlishWDev.com'+'</h1>') //2nd type
+
+
+// Space between sentenec
+// var fistName = 'Mujeeb'
+// var lastName = 'Rehman'
+// document.write(fistName+" "+lastName)
+
+// vareable for number
+var a = 10
+var b = 20
+var c = a+b
+document.write(a+b+c)
+
